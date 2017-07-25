89ef92756237d7216303dd4654feb5750cbe2e8b
Khry <khrustyk@gmail.com>
2017-07-25 22:02:01 +0300

    add2

--- script/decypherPhrase.js
+++ script/decypherPhrase.js
@@ -1,15 +1,13 @@
 //5________________________________________________________
-
-function newmap(map) {
-    var newMap = Object.keys(map).reduce(function(obj,key) {
-       obj[ map[key] ] = key;
-       return obj;
-    },{});
-    return newMap;
-}
 var charactersMap = {a: 'o', c: 'd', t: 'g'}
-function decypherPhrase(object, str) {
-    var newObj = newmap(charactersMap);
-    return cypherPhrase(newObj, str);
-};
+
+function decypherPhrase (obj, str) {
+   var newObj = {};
+   for (var prop in obj) {
+       if(obj.hasOwnProperty(prop)) {
+           newObj[obj[prop]] = prop;
+       }
+   }
+   return cypherPhrase (newObj, str);
+}
 console.log(decypherPhrase( charactersMap, 'kiggy dog' )); // -> “kitty cat”
